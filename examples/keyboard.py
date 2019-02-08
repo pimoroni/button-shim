b2ac04b4ed42077c27e5f5f57d85c8402dfde732
Phil Howard <phil@gadgetoid.com>
2019-02-08 13:37:41 +0000

    Code QA and stickler linting

--- examples/keyboard.py
+++ examples/keyboard.py
@@ -10,6 +10,7 @@ except ImportError:
     exit("This library requires the evdev module\nInstall with: sudo pip install evdev")
 
 KEYCODES = [e.KEY_A, e.KEY_B, e.KEY_C, e.KEY_D, e.KEY_E]
+BUTTONS = [buttonshim.BUTTON_A, buttonshim.BUTTON_B, buttonshim.BUTTON_C, buttonshim.BUTTON_D, buttonshim.BUTTON_E]
 
 print("""
 Button SHIM: keyboard.py
@@ -29,19 +30,20 @@ except uinput.UInputError as e:
     sys.exit(0)
 
 
-@buttonshim.on_press([buttonshim.BUTTON_A, buttonshim.BUTTON_B, buttonshim.BUTTON_C, buttonshim.BUTTON_D, buttonshim.BUTTON_E])
+@buttonshim.on_press(BUTTONS)
 def button_p_handler(button, pressed):
     keycode = KEYCODES[button]
     print("Press: {}".format(keycode))
     ui.write(e.EV_KEY, keycode, 1)
     ui.syn()
 
-@buttonshim.on_release([buttonshim.BUTTON_A, buttonshim.BUTTON_B, buttonshim.BUTTON_C, buttonshim.BUTTON_D, buttonshim.BUTTON_E])
+
+@buttonshim.on_release(BUTTONS)
 def button_r_handler(button, pressed):
     keycode = KEYCODES[button]
     print("Release: {}".format(keycode))
     ui.write(e.EV_KEY, keycode, 0)
     ui.syn()
 
-signal.pause()
 
+signal.pause()
--- examples/rainbow.py
+++ examples/rainbow.py
@@ -12,24 +12,30 @@ Press Ctrl+C to exit.
 
 """)
 
+
 @buttonshim.on_press(buttonshim.BUTTON_A)
 def button_a(button, pressed):
     buttonshim.set_pixel(0x94, 0x00, 0xd3)
+
 
 @buttonshim.on_press(buttonshim.BUTTON_B)
 def button_b(button, pressed):
     buttonshim.set_pixel(0x00, 0x00, 0xff)
 
+
 @buttonshim.on_press(buttonshim.BUTTON_C)
 def button_c(button, pressed):
     buttonshim.set_pixel(0x00, 0xff, 0x00)
+
 
 @buttonshim.on_press(buttonshim.BUTTON_D)
 def button_d(button, pressed):
     buttonshim.set_pixel(0xff, 0xff, 0x00)
 
+
 @buttonshim.on_press(buttonshim.BUTTON_E)
 def button_e(button, pressed):
     buttonshim.set_pixel(0xff, 0x00, 0x00)
+
 
 signal.pause()
